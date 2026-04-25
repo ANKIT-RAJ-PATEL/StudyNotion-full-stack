76406cc535dd17a3389c6f6e791a1b4afcc6d4a9
ANKIT-RAJ-PATEL <ankitraj61370@gmail.com>
2026-04-25 12:26:29 +0530

    8th

--- src/Components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ src/Components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -28,7 +28,7 @@ const RenderTotalAmount = () => {
   return (
     <div className='min-w-[280px] rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-3 md:p-6 '>
 
-        <p className='mb-1 text-sm font-medium text-richblack-300'>Total:</p>
+        <p className='mb-1 text-sm font-medium text-richblack-300'>ToTal:</p>
         <p className='mb-6 text-3xl font-medium text-yellow-100 crimson'>₹ {total}</p>
 
         <IconBtn 
